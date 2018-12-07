9861021a98cd0ca900a878ff6b6187ddb43eea78
Nachiappan <nachiappan.nk.ext@sivantos.com>
2018-12-07 22:10:02 +0530

    Updated 4 usages

---
+++ JsonSectionReaderUsage/JsonSectionReaderUsage/Example3Encoding.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using WonderTools.JsonSectionReader;
+
+namespace JsonSectionReaderUsage
+{
+    [TestFixture]
+    public class Example3Encoding
+    {
+        [Test]
+        public void Test()
+        {
+            var word = "Mädchen";
+            var actualWord = JSectionReader.Section("Example3Encoding.json").GetSection("words", 2).GetObject<string>();
+            Assert.AreEqual(word, actualWord);
+        }
+    }
+}
