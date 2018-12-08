d4129c0d45f59dcee2c2c1c023c1ee50b3d684ba
Nachiappan <nachiappan.nk.ext@sivantos.com>
2018-12-08 18:20:38 +0530

    Reading table as a list of objects

--- JsonSectionReaderUsage/JsonSectionReaderUsage/Example9ListOfObject.cs
+++ JsonSectionReaderUsage/JsonSectionReaderUsage/Example9ListOfObject.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 using WonderTools.JsonSectionReader;
 
@@ -10,14 +11,27 @@ namespace JsonSectionReaderUsage
         [Test]
         public void Test()
         {
-            var data = new JSectionReader().Read("TableDataTests.json").GetSection("data")
+            var employees = new JSectionReader().Read("Example9ListOfObject.json").GetSection("data")
                 .GetTableAsObjectList<Employee, int, string, int>(
-                    (id, name, age) => { return new Employee()
+                    (id, name, age) => new Employee()
                     {
                         Id = id,
                         Name = name,
                         Age = age,
-                    }; });
+                    });
+            var expectedEmployees = new List<Employee>()
+            {
+                new Employee(){Id =2432,Name ="John", Age = 32},
+                new Employee(){Id =2222,Name ="Nash", Age = 33},
+                new Employee(){Id =3421,Name ="Peter", Age = 33},
+            };
+
+            for (int i = 0; i < expectedEmployees.Count; i++)
+            {
+                Assert.AreEqual(expectedEmployees[i].Id, employees[i].Id);
+                Assert.AreEqual(expectedEmployees[i].Name, employees[i].Name);
+                Assert.AreEqual(expectedEmployees[i].Age, employees[i].Age);
+            }
         }
     }
 
