4e3f1128c4fe0496e85e40302db24cf76971fee9
Nachiappan <nachiappan.nk.ext@sivantos.com>
2018-12-08 18:12:55 +0530

    Implemented list of list usage

--- JsonSectionReaderUsage/JsonSectionReaderUsage/Example8ListOfList.cs
+++ JsonSectionReaderUsage/JsonSectionReaderUsage/Example8ListOfList.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 using WonderTools.JsonSectionReader;
 
 namespace JsonSectionReaderUsage
@@ -10,7 +11,23 @@ namespace JsonSectionReaderUsage
         public void Test()
         { 
             var data = new JSectionReader().Read("Example8ListOfList.json").GetSection("data")
-                .GetTable(typeof(int), typeof(string), typeof(int));
+                .GetTable(typeof(int), typeof(string), typeof(string));
+
+            var expectedData = new List<List<object>>()
+            {
+                new List<object>() {1, "Monday", "Morning"},
+                new List<object>() {2, "Monday", "Afternoon"},
+                new List<object>() {3, "Tuesday", "Morning"},
+                new List<object>() {4, "Tuesday", "Afternoon"},
+            };
+
+            for (int i = 0; i < expectedData.Count; i++)
+            {
+                for (int j = 0; j < expectedData[i].Count; j++)
+                {
+                    Assert.AreEqual(data[i][j],expectedData[i][j]);
+                }
+            }
         }
     }
 }
